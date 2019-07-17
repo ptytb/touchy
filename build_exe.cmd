df6798fa83759fbd5954851f0aeff45476075a80
Ilya Pronin <studst2016@ya.ru>
2019-07-17 21:41:51 +0300

    *call vcvars64.bat

--- build_exe.cmd
+++ build_exe.cmd
@@ -1,3 +1,4 @@
+rem call me from `pipenv shell`
 set CL=/Zm2000
-%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 nuitka --jobs=1 --show-progress --show-scons --standalone --windows-disable-console --python-flag=no_site --output-dir=dist  --include-module=autoclass  --include-module=valid8  --include-module=mini_lambda --include-module=six --include-module=mido.backends.rtmidi --include-module=rtmidi touchy.py
