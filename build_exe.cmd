29d2517d4c83b6de256b79d7fa5003c3b8434719
Ilya Pronin <studst2016@ya.ru>
2019-07-17 21:16:30 +0300

    -disable console for exe app

--- build_exe.cmd
+++ build_exe.cmd
@@ -1,3 +1,3 @@
 set CL=/Zm2000
 %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-nuitka --jobs=1 --show-progress --show-scons --standalone --python-flag=no_site --output-dir=dist  --include-module=autoclass  --include-module=valid8  --include-module=mini_lambda --include-module=six --include-module=mido.backends.rtmidi --include-module=rtmidi touchy.py
+nuitka --jobs=1 --show-progress --show-scons --standalone --windows-disable-console --python-flag=no_site --output-dir=dist  --include-module=autoclass  --include-module=valid8  --include-module=mini_lambda --include-module=six --include-module=mido.backends.rtmidi --include-module=rtmidi touchy.py
